e448cfc40d32870a238891ef554544fc39e95369
marat569 <marat569@gmail.com>
2024-10-28 21:57:36 -0400

    [RYZA3] Update links

--- src/games/ryza3/addon.cpp
+++ src/games/ryza3/addon.cpp
@@ -7,25 +7,20 @@
 
 #define DEBUG_LEVEL_0
 
-//#define DEBUG_LEVEL_1 //added
+// #define DEBUG_LEVEL_1 //added
 
-#include <embed/0x006F1991.h> // UI, Overworld
-#include <embed/0x3387BE75.h> // UI, Portraits
-#include <embed/0xD9560318.h> // UI, Chat Bubbles
-#include <embed/0xCE8ED088.h> // Videos 1
-#include <embed/0xE7C986EB.h> // Videos 2
-#include <embed/0x21E7062A.h> // Combat Artifacts
-#include <embed/0x9F6B73CA.h> // Combat/ui artifacts 2
-#include <embed/0x892226E0.h> // ui artifacts
-#include <embed/0xD13E98FA.h> // wardrobe tonemapper
-#include <embed/0xDA0B69B5.h> // tonemapper TAA/AA OFF
-#include <embed/0xD87A1EAB.h> // tonemapper FXAA
-#include <embed/0xBB055A34.h> // Final
-
-
-
-
-
+#include <embed/0x006F1991.h>  // UI, Overworld
+#include <embed/0x21E7062A.h>  // Combat Artifacts
+#include <embed/0x3387BE75.h>  // UI, Portraits
+#include <embed/0x892226E0.h>  // ui artifacts
+#include <embed/0x9F6B73CA.h>  // Combat/ui artifacts 2
+#include <embed/0xBB055A34.h>  // Final
+#include <embed/0xCE8ED088.h>  // Videos 1
+#include <embed/0xD13E98FA.h>  // wardrobe tonemapper
+#include <embed/0xD87A1EAB.h>  // tonemapper FXAA
+#include <embed/0xD9560318.h>  // UI, Chat Bubbles
+#include <embed/0xDA0B69B5.h>  // tonemapper TAA/AA OFF
+#include <embed/0xE7C986EB.h>  // Videos 2
 
 #include <deps/imgui/imgui.h>
 #include <include/reshade.hpp>
@@ -39,19 +34,18 @@ namespace {
 
 renodx::mods::shader::CustomShaders custom_shaders = {
 
-    CustomShaderEntry(0x006F1991), //UI, Overworld
-    CustomShaderEntry(0x3387BE75),  // UI, Portraits
-    CustomShaderEntry(0xD9560318),    // UI, Chat Bubbles
-    CustomShaderEntry(0xCE8ED088),  // Videos 1
-    CustomSwapchainShader(0xE7C986EB), // Videos 2
-    CustomShaderEntry(0x21E7062A),  // Combat Artifacts
-	  CustomShaderEntry(0x9F6B73CA), // Combat artifacts 2
-	  CustomShaderEntry(0x892226E0), // UI Artifacts
-    CustomShaderEntry(0xD13E98FA), // Wardrobe tonemapper
-    CustomShaderEntry(0xDA0B69B5),  // Tonemapper!! [TAA/AA off]
-    CustomShaderEntry(0xD87A1EAB), // Tonemapper!! [FXAA]
-    CustomShaderEntry(0xBB055A34), // Final
-	
+    CustomShaderEntry(0x006F1991),      // UI, Overworld
+    CustomShaderEntry(0x3387BE75),      // UI, Portraits
+    CustomShaderEntry(0xD9560318),      // UI, Chat Bubbles
+    CustomShaderEntry(0xCE8ED088),      // Videos 1
+    CustomSwapchainShader(0xE7C986EB),  // Videos 2
+    CustomShaderEntry(0x21E7062A),      // Combat Artifacts
+    CustomShaderEntry(0x9F6B73CA),      // Combat artifacts 2
+    CustomShaderEntry(0x892226E0),      // UI Artifacts
+    CustomShaderEntry(0xD13E98FA),      // Wardrobe tonemapper
+    CustomShaderEntry(0xDA0B69B5),      // Tonemapper!! [TAA/AA off]
+    CustomShaderEntry(0xD87A1EAB),      // Tonemapper!! [FXAA]
+    CustomShaderEntry(0xBB055A34),      // Final
 
 };
 
@@ -112,7 +106,6 @@ renodx::utils::settings::Settings settings = {
         .tooltip = "Will make sure the output never exceeds your set peak brightness",
     },
 
-
     new renodx::utils::settings::Setting{
         .key = "colorGradeExposure",
         .binding = &shader_injection.colorGradeExposure,
@@ -159,7 +152,7 @@ renodx::utils::settings::Settings settings = {
         .parse = [](float value) { return value * 0.02f; },
     },
 
-     new renodx::utils::settings::Setting{
+    new renodx::utils::settings::Setting{
         .key = "colorGradeBlowout",
         .binding = &shader_injection.colorGradeBlowout,
         .default_value = 50.f,
@@ -170,7 +163,7 @@ renodx::utils::settings::Settings settings = {
         .parse = [](float value) { return value * 0.01f; },
     },
 
-     new renodx::utils::settings::Setting{
+    new renodx::utils::settings::Setting{
         .key = "toneMapHueCorrection",
         .binding = &shader_injection.toneMapHueCorrection,
         .default_value = 50.f,
@@ -180,7 +173,6 @@ renodx::utils::settings::Settings settings = {
         .max = 100.f,
         .parse = [](float value) { return value * 0.01f; },
     },
-
 
     new renodx::utils::settings::Setting{
         .key = "blend",
@@ -202,10 +194,10 @@ renodx::utils::settings::Settings settings = {
         .label = "Bloom + DoF",
         .section = "Effects",
         .tooltip = "Enable/Disable Bloom + Depth of Field",
-       
+
     },
 
-     new renodx::utils::settings::Setting{
+    new renodx::utils::settings::Setting{
         .key = "fxBloom",
         .binding = &shader_injection.fxBloom,
         .default_value = 50.f,
@@ -215,7 +207,6 @@ renodx::utils::settings::Settings settings = {
         .max = 100.f,
         .parse = [](float value) { return value * 0.02f; },
     },
-
 
     new renodx::utils::settings::Setting{
         .key = "chromaticAberration",
@@ -242,7 +233,19 @@ renodx::utils::settings::Settings settings = {
         .group = "button-line-1",
         .tint = 0x5865F2,
         .on_change = []() {
-          system("start https://discord.gg/5WZXDpmbpP");
+          static const std::string obfuscated_link = std::string("start https://discord.gg/5WZX") + std::string("DpmbpP");
+          system(obfuscated_link.c_str());
+        },
+    },
+
+    new renodx::utils::settings::Setting{
+        .value_type = renodx::utils::settings::SettingValueType::BUTTON,
+        .label = "Get more RenoDX mods!",
+        .section = "About",
+        .group = "button-line-1",
+        .tint = 0x5865F2,
+        .on_change = []() {
+          system("start https://github.com/clshortfuse/renodx/wiki/Mods");
         },
     },
 
@@ -262,7 +265,7 @@ void OnPresetOff() {
   renodx::utils::settings::UpdateSetting("colorGradeBlowout", 50.f);
   renodx::utils::settings::UpdateSetting("toneMapHueCorrection", 50.f);
   renodx::utils::settings::UpdateSetting("blend", 0);
-  //Start PostProcess effects on/off
+  // Start PostProcess effects on/off
   renodx::utils::settings::UpdateSetting("bloom", 1);
   renodx::utils::settings::UpdateSetting("fxBloom", 50.f);
   renodx::utils::settings::UpdateSetting("chromaticAberration", 1);
@@ -281,32 +284,28 @@ BOOL APIENTRY DllMain(HMODULE h_module, DWORD fdw_reason, LPVOID lpv_reserved) {
   switch (fdw_reason) {
     case DLL_PROCESS_ATTACH:
       if (!reshade::register_addon(h_module)) return FALSE;
-      renodx::mods::shader::force_pipeline_cloning = true; //So the mod works with the toolkit
+      renodx::mods::shader::force_pipeline_cloning = true;  // So the mod works with the toolkit
 
-      renodx::mods::swapchain::force_borderless = false; //needed for stability
-      renodx::mods::swapchain::prevent_full_screen = false; //needed for stability
+      renodx::mods::swapchain::force_borderless = false;     // needed for stability
+      renodx::mods::swapchain::prevent_full_screen = false;  // needed for stability
 
+      // BGRA8_typeless needed to uncap luminance
 
-       // BGRA8_typeless needed to uncap luminance
-      
       // BGRA8_typeless
       renodx::mods::swapchain::swap_chain_upgrade_targets.push_back({
           .old_format = reshade::api::format::b8g8r8a8_typeless,
           .new_format = reshade::api::format::r16g16b16a16_float,
-    //   .index = 39, //Maybe find the specific render target that uncaps the game one day, but not right now
-    //   .ignore_size = true, //Ignoring size allows you to uncap when the game runs in a sub-native resolution, but tons of artifacts are created
+          //   .index = 39, //Maybe find the specific render target that uncaps the game one day, but not right now
+          //   .ignore_size = true, //Ignoring size allows you to uncap when the game runs in a sub-native resolution, but tons of artifacts are created
       });
 
-
-     // BGRA8_unorm
-    //  renodx::mods::swapchain::swap_chain_upgrade_targets.push_back({
-     //    .old_format = reshade::api::format::b8g8r8a8_unorm,
-     //    .new_format = reshade::api::format::r16g16b16a16_float,
-     //  .index = 39,
-     //.ignore_size = true,
-    //  });
-
-
+      // BGRA8_unorm
+      //  renodx::mods::swapchain::swap_chain_upgrade_targets.push_back({
+      //    .old_format = reshade::api::format::b8g8r8a8_unorm,
+      //    .new_format = reshade::api::format::r16g16b16a16_float,
+      //  .index = 39,
+      //.ignore_size = true,
+      //  });
 
       break;
     case DLL_PROCESS_DETACH:

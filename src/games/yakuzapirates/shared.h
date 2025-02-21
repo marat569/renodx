8aa994e8308ae574815e6cf0a131483fd8713313
marat569 <marat569@gmail.com>
2025-02-20 22:37:44 -0500

    [YAKUZAPIRATES] Initial Upload
    - Initial Yakuza Pirates in Hawaii upload

---
+++ src/games/yakuzapirates/shared.h
@@ -0,0 +1,36 @@
+#ifndef SRC_YAKPIRATE_SHARED_H_
+#define SRC_YAKPIRATE_SHARED_H_
+
+#ifndef __cplusplus
+#include "../../shaders/renodx.hlsl"
+#endif
+
+// Must be 32bit aligned
+// Should be 4x32
+struct ShaderInjectData {
+  float toneMapType;
+  float toneMapPeakNits;
+  float toneMapGameNits;
+  float toneMapUINits;
+  float colorGradeExposure;
+  float colorGradeHighlights;
+  float colorGradeShadows;
+  float colorGradeContrast;
+  float colorGradeSaturation;
+  float toneMapHueCorrectionStrength;
+  float toneMapHueCorrectionType;
+  float AutoHDR;
+  float ColorGradeColorSpace;
+};
+
+// cbuffer cb13 : register(b13) {
+//   ShaderInjectData injectedData : packoffset(c0);
+// }
+#ifndef __cplusplus
+cbuffer injectedBuffer : register(b0, space50) {
+  ShaderInjectData injectedData : packoffset(c0);
+}
+
+#endif
+
+#endif  // SRC_YAKPIRATE_SHARED_H_

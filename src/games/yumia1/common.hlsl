fd7aab860498629ccbd1ad7536c401fec84e55a5
marat569 <marat569@gmail.com>
2025-03-21 15:31:10 -0400

    [YUMIA1] fix stuff up
    - tonemap pass was fixed, no longer need to feed neutralsdr as the third param

--- src/games/yumia1/common.hlsl
+++ src/games/yumia1/common.hlsl
@@ -6,9 +6,8 @@ float4 ProcessColor(float3 untonemapped, float3 graded) {
 
   if (RENODX_TONE_MAP_TYPE != 0.f) {
     // untonemapped.rgb *= midGray / 0.18f; // Adjust midgray, RenoDRT except 0.18f
-    float3 neutral_sdr = renodx::tonemap::renodrt::NeutralSDR(untonemapped);
 
-    color.rgb = renodx::draw::ToneMapPass(untonemapped, graded, neutral_sdr);
+    color.rgb = renodx::draw::ToneMapPass(untonemapped, graded);
     color.rgb = renodx::color::correct::GammaSafe(color.rgb);
     color.rgb *= RENODX_GAME_NITS / RENODX_UI_NITS;
     color.rgb = renodx::color::correct::GammaSafe(color.rgb, true);

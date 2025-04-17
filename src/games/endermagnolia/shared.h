63960bca022bb2f90a23d0ce4bd741feea8834f2
Mohanned Hassan <gailardiagalane@gmail.com>
2025-04-17 14:44:26 +0400

    feat(endermagnolia): Add per channel correction sliders

--- src/games/endermagnolia/shared.h
+++ src/games/endermagnolia/shared.h
@@ -2,29 +2,32 @@
 #ifndef SRC_ENDERMAG_SHARED_H_
 #define SRC_ENDERMAG_SHARED_H_
 
-#define RENODX_TONE_MAP_TYPE                 shader_injection.toneMapType
-#define RENODX_PEAK_NITS                     shader_injection.toneMapPeakNits
-#define RENODX_GAME_NITS                     shader_injection.toneMapGameNits
-#define RENODX_UI_NITS                       shader_injection.toneMapUINits
-#define RENODX_COLOR_GRADE_STRENGTH          shader_injection.colorGradeStrength
-#define RENODX_TONE_MAP_EXPOSURE             shader_injection.colorGradeExposure
-#define RENODX_TONE_MAP_HIGHLIGHTS           shader_injection.colorGradeHighlights
-#define RENODX_TONE_MAP_SHADOWS              shader_injection.colorGradeShadows
-#define RENODX_TONE_MAP_CONTRAST             shader_injection.colorGradeContrast
-#define RENODX_TONE_MAP_SATURATION           shader_injection.colorGradeSaturation
-#define RENODX_TONE_MAP_HIGHLIGHT_SATURATION shader_injection.colorGradeHighlightSaturation
-#define RENODX_TONE_MAP_BLOWOUT              shader_injection.colorGradeBlowout
-#define RENODX_TONE_MAP_FLARE                shader_injection.colorGradeFlare
-#define RENODX_TONE_MAP_WORKING_COLOR_SPACE  color::convert::COLOR_SPACE_AP1
-#define RENODX_TONE_MAP_PER_CHANNEL          shader_injection.toneMapPerChannel
-#define RENODX_TONE_MAP_HUE_PROCESSOR        shader_injection.toneMapHueProcessor
-#define RENODX_TONE_MAP_HUE_CORRECTION       shader_injection.toneMapHueCorrection
-#define RENODX_TONE_MAP_HUE_SHIFT            shader_injection.toneMapHueShift
-#define RENODX_TONE_MAP_CLAMP_COLOR_SPACE    color::convert::COLOR_SPACE_BT2020
-#define RENODX_RENO_DRT_TONE_MAP_METHOD      renodx::tonemap::renodrt::config::tone_map_method::REINHARD
-#define RENODX_GAMMA_CORRECTION              shader_injection.toneMapGammaCorrection
-#define RENODX_SWAP_CHAIN_CUSTOM_COLOR_SPACE shader_injection.colorGradeColorSpace
-#define RENODX_SWAP_CHAIN_CLAMP_COLOR_SPACE  color::convert::COLOR_SPACE_BT2020
+#define RENODX_TONE_MAP_TYPE                      shader_injection.toneMapType
+#define RENODX_PEAK_NITS                          shader_injection.toneMapPeakNits
+#define RENODX_GAME_NITS                          shader_injection.toneMapGameNits
+#define RENODX_UI_NITS                            shader_injection.toneMapUINits
+#define RENODX_COLOR_GRADE_STRENGTH               shader_injection.colorGradeStrength
+#define RENODX_TONE_MAP_EXPOSURE                  shader_injection.colorGradeExposure
+#define RENODX_TONE_MAP_HIGHLIGHTS                shader_injection.colorGradeHighlights
+#define RENODX_TONE_MAP_SHADOWS                   shader_injection.colorGradeShadows
+#define RENODX_TONE_MAP_CONTRAST                  shader_injection.colorGradeContrast
+#define RENODX_TONE_MAP_SATURATION                shader_injection.colorGradeSaturation
+#define RENODX_TONE_MAP_HIGHLIGHT_SATURATION      shader_injection.colorGradeHighlightSaturation
+#define RENODX_TONE_MAP_BLOWOUT                   shader_injection.colorGradeBlowout
+#define RENODX_TONE_MAP_FLARE                     shader_injection.colorGradeFlare
+#define RENODX_TONE_MAP_WORKING_COLOR_SPACE       color::convert::COLOR_SPACE_AP1
+#define RENODX_TONE_MAP_PER_CHANNEL               shader_injection.toneMapPerChannel
+#define RENODX_TONE_MAP_HUE_PROCESSOR             shader_injection.toneMapHueProcessor
+#define RENODX_TONE_MAP_HUE_CORRECTION            shader_injection.toneMapHueCorrection
+#define RENODX_TONE_MAP_HUE_SHIFT                 shader_injection.toneMapHueShift
+#define RENODX_TONE_MAP_CLAMP_COLOR_SPACE         color::convert::COLOR_SPACE_BT2020
+#define RENODX_RENO_DRT_TONE_MAP_METHOD           renodx::tonemap::renodrt::config::tone_map_method::REINHARD
+#define RENODX_GAMMA_CORRECTION                   shader_injection.toneMapGammaCorrection
+#define RENODX_SWAP_CHAIN_CUSTOM_COLOR_SPACE      shader_injection.colorGradeColorSpace
+#define RENODX_SWAP_CHAIN_CLAMP_COLOR_SPACE       color::convert::COLOR_SPACE_BT2020
+#define RENODX_PER_CHANNEL_HUE_CORRECTION         shader_injection.color_grade_hue_correction
+#define RENODX_PER_CHANNEL_CHROMINANCE_CORRECTION shader_injection.color_grade_saturation_correction
+#define RENODX_PER_CHANNEL_BLOWOUT_RESTORATION    shader_injection.color_grade_blowout_restoration
 // #define RENODX_SWAP_CHAIN_ENCODING_COLOR_SPACE (color::convert::COLOR_SPACE_BT2020 - shader_injection.processingUseSCRGB)  // BT709 = BT2020 - 1
 // #define RENODX_SWAP_CHAIN_ENCODING             (ENCODING_PQ + shader_injection.processingUseSCRGB)                         // SCRGB = PQ + 1
 #define RENODX_SWAP_CHAIN_ENCODING_COLOR_SPACE (color::convert::COLOR_SPACE_BT2020 - 1.f)  // BT709 = BT2020 - 1
@@ -57,6 +60,10 @@ struct ShaderInjectData {
   float colorGradeColorSpace;
   float colorGradeRestorationMethod;
   float colorGradeStrength;
+
+  float color_grade_hue_correction;
+  float color_grade_saturation_correction;
+  float color_grade_blowout_restoration;
 };
 
 #ifndef __cplusplus

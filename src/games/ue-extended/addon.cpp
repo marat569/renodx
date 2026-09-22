06b670f843c12f82a5bae8a5719e7dc914d11e58
marat569 <marat569@gmail.com>
2026-09-22 16:42:29 -0400

    [UE-EXTENDED] Default SH Townfall to upgrade path off

--- src/games/ue-extended/addon.cpp
+++ src/games/ue-extended/addon.cpp
@@ -1545,6 +1545,12 @@ const std::unordered_map<std::string, GameSettings> GAME_SETTINGS = {
             {"Set_Path", 0.f},
         },
     },
+    {
+        "Townfall",
+        GameSettings{
+            {"Set_Path", 0.f},
+        },
+    },
 };
 
 auto FindGameSettings(const std::filesystem::path& process_path) {
